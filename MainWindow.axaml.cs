0c4329f52f59c6033b1eb8f0229e4f943d8790d4
Daniel <dpenalba@codicesoftware.com>
2021-12-28 11:03:58 +0100

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace AvaloniaImageProcessing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            DragDrop.SetAllowDrop(this, true);
+
+            AddHandler(DragDrop.DragEnterEvent, OnDragEnter);
+            //AddHandler(DragDrop.DragOverEvent, OnDragOver);
+            AddHandler(DragDrop.DragLeaveEvent, OnDragLeave);
+            AddHandler(DragDrop.DropEvent, OnDrop);
+
+            mImagePanel = new ImagePanel();
+            Content = mImagePanel;
+        }
+
+        private void OnDragLeave(object? sender, RoutedEventArgs e)
+        {
+            Background = Brushes.Transparent;
+        }
+
+        private void OnDragEnter(object? sender, DragEventArgs e)
+        {
+            Background = Brushes.LightGray;
+        }
+
+        private void OnDrop(object? sender, DragEventArgs e)
+        {
+            var file = e.Data.GetFileNames()?.ToList()[0];
+
+            if (file == null)
+                return;
+
+            using (var stream = System.IO.File.OpenRead(file))
+            {
+                var image = WriteableBitmap.Decode(stream);
+                Width = image.Size.Width;
+                Height = image.Size.Height;
+                mImagePanel.Image = image;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        class ImagePanel : Panel
+        {
+            public WriteableBitmap Image
+            {
+                get
+                {
+                    return mImage;
+                }
+                set
+                {
+                    mImage = value;
+                    mCopyImage = new WriteableBitmap(
+                        mImage.PixelSize,
+                        mImage.Dpi,
+                        PixelFormat.Bgra8888,
+                        AlphaFormat.Premul);
+                }
+            }
+
+            public override void Render(DrawingContext context)
+            {
+                base.Render(context);
+
+                if (Image == null)
+                    return;
+
+                ReDrawImage();
+
+                context.DrawImage(mCopyImage, new Rect(0, 0, mCopyImage.Size.Width, mCopyImage.Size.Height));
+            }
+
+            protected override void OnPointerMoved(PointerEventArgs e)
+            {
+                base.OnPointerMoved(e);
+                mLastMousePoint = e.GetPosition(this);
+                InvalidateVisual();
+            }
+
+            private unsafe void ReDrawImage()
+            {
+                using (var leftBuffer = mImage.Lock())
+                using (var copyBuffer = mCopyImage.Lock())
+                {
+                    byte* buffer = (byte*)leftBuffer.Address.ToPointer();
+                    byte* copybuffer = (byte*)copyBuffer.Address.ToPointer();
+
+                    for (int x = 0; x < mImage.Size.Width; x++)
+                    {
+                        for (int y = 0; y < mImage.Size.Height; y++)
+                        {
+                            int loc = (int)(x + y * mImage.Size.Width);
+
+                            byte b = buffer[loc * 4];
+                            byte g = buffer[loc * 4 + 1];
+                            byte r = buffer[loc * 4 + 2];
+                            byte a = buffer[loc * 4 + 3];
+
+                            /*int brightness = Brightness(r, g, b);
+
+                            byte targetColor;
+
+                            if (brightness > mLastMousePoint.X)
+                                targetColor = 255;
+                            else
+                                targetColor = 0;
+
+                            copybuffer[loc * 4] = targetColor;
+                            copybuffer[loc * 4 + 1] = targetColor;
+                            copybuffer[loc * 4 + 2] = targetColor;
+                            copybuffer[loc * 4 + 3] = a;*/
+
+                            double distance = mLastMousePoint.Distance(x, y);
+                            double factor = distance.Map(0, 200, 2, 0);
+
+                            copybuffer[loc * 4] = Clamp(b * factor, 0, 255);
+                            copybuffer[loc * 4 + 1] = Clamp(g * factor, 0, 255);
+                            copybuffer[loc * 4 + 2] = Clamp(r * factor, 0, 255);
+                            copybuffer[loc * 4 + 3] = a;
+                        }
+                    }
+                }
+            }
+
+            private byte Clamp(double value, double min, double max)
+            {
+                if (value < min)
+                    return (byte)min;
+                if (value > max)
+                    return (byte)max;
+                return (byte)value;
+            }
+
+            private static byte Brightness(byte r, byte g, byte b)
+            {
+                return (byte)((r + g + b) / 3);
+            }
+
+            Point mLastMousePoint;
+            WriteableBitmap mImage;
+            WriteableBitmap mCopyImage;
+        }
+        ImagePanel mImagePanel;
+    }
+
+        static class ExtensionMethods
+        {
+            public static double Map (this double value, double fromSource, double toSource, double fromTarget, double toTarget)
+            {
+                return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+            }
+
+            public static double Distance(this Point p1, Point p2)
+            {
+                return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+            }
+
+            public static double Distance(this Point p1, double x, double y)
+            {
+                return Math.Sqrt(Math.Pow(p1.X - x, 2) + Math.Pow(p1.Y - y, 2));
+            }
+        }
+}
